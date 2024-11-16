1f0742bd1abcd3a9788dafa8c84e1fd077c0feb6
lightyears <lightyears1998@hotmail.com>
2024-11-16 13:57:21 +0800

    feat: 居中窗口

--- EgoPrimer/Views/MainWindow.axaml.cs
+++ EgoPrimer/Views/MainWindow.axaml.cs
@@ -7,5 +7,6 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        WindowStartupLocation = WindowStartupLocation.CenterScreen;
     }
 }
