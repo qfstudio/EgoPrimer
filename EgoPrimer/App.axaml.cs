bda552f016dffcc86eb3389c962b6ba273a27e63
lightyears <lightyears1998@hotmail.com>
2025-02-28 19:24:30 +0800

    feat: introduce EFCore

--- EgoPrimer/App.axaml.cs
+++ EgoPrimer/App.axaml.cs
@@ -1,10 +1,23 @@
 using Avalonia;
+using Avalonia.Collections;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using EgoPrimer.ViewModels;
 using EgoPrimer.Views;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace EgoPrimer;
+
+static class ServiceCollectionExtensions
+{
+    public static void AddCommonServices(this IServiceCollection collection)
+    {
+        collection.AddTransient<MainViewModel>();
+        
+        collection.AddTransient<MainWindow>();
+        collection.AddTransient<MainView>();
+    }
+}
 
 public partial class App : Application
 {
@@ -15,18 +28,24 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        var collection = new ServiceCollection();
+        collection.AddCommonServices();
+
+        var serviceProvider = collection.BuildServiceProvider();
+        var vm = serviceProvider.GetRequiredService<MainViewModel>();
+        
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainViewModel()
+                DataContext = vm
             };
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
             singleViewPlatform.MainView = new MainView
             {
-                DataContext = new MainViewModel()
+                DataContext = vm
             };
         }
 
