7fb6c76eddad8e291556c4bdb8dc2077d4777659
lightyears <lightyears1998@hotmail.com>
2025-03-13 00:21:45 +0800

    feat: Items repeating

--- EgoPrimer/ViewModels/ISceneViewModel.cs
+++ EgoPrimer/ViewModels/ISceneViewModel.cs
@@ -1,24 +1,11 @@
-﻿using System;
-using System.Reactive.Subjects;
-using Avalonia.Controls;
-
-namespace EgoPrimer.ViewModels;
-
-public class NavigationRequest(Type destination)
-{
-    public Type Destination { get; } = destination;
-}
+﻿namespace EgoPrimer.ViewModels;
 
 public interface ISceneViewModel
 {
     string Name { get; }
-    
-    Subject<NavigationRequest> NavigationRequest { get; }
 }
 
 public abstract class SceneViewModelBase : ViewModelBase, ISceneViewModel
 {
     public abstract string Name { get; }
-    
-    public Subject<NavigationRequest> NavigationRequest { get; } = new();
 }
--- EgoPrimer/Views/NavigationBar.axaml.cs
+++ EgoPrimer/Views/NavigationBar.axaml.cs
@@ -1,10 +1,12 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using EgoPrimer.ViewModels;
 
 namespace EgoPrimer.Views;
 
-public partial class NavigationBar : UserControl
+public partial class NavigationBar : ReactiveUserControl<MainViewModel>
 {
     public NavigationBar()
     {
