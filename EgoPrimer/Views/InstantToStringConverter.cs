5880410e4f3edbd6c0e3c1d6f0c1d33bf7bb9242
lightyears <lightyears1998@hotmail.com>
2025-03-17 22:32:19 +0800

    feat: IConverter

--- EgoPrimer/Views/InstantToStringConverter.cs
+++ EgoPrimer/Views/InstantToStringConverter.cs
@@ -1,5 +1,7 @@
 ﻿using System.Globalization;
 using Avalonia.Data.Converters;
+using NodaTime;
+using NodaTime.Text;
 
 namespace EgoPrimer.Views;
 
@@ -7,7 +9,15 @@ public class InstantToStringConverter : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        if (value is Instant instant)
+        {
+            var systemZone = DateTimeZoneProviders.Tzdb.GetSystemDefault();
+            var zonedDateTime = instant.InZone(systemZone);
+            var pattern = ZonedDateTimePattern.CreateWithInvariantCulture("yyyy-MM-dd HH:mm:ss zzz", DateTimeZoneProviders.Tzdb);
+            return pattern.Format(zonedDateTime);
+        }
+
+        return null;
     }
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
