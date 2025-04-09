cb49e9647a1744d8fb8e4e2a82502ba2cb309077
lightyears <lightyears1998@hotmail.com>
2025-04-09 13:59:22 +0800

    dep: Introduce LiveCharts2

--- EgoPrimer/ViewModels/HealthSceneViewModel.cs
+++ EgoPrimer/ViewModels/HealthSceneViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace EgoPrimer.ViewModels;
+﻿using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Drawing.Geometries;
+
+namespace EgoPrimer.ViewModels;
 
 public class HealthSceneViewModel : SceneViewModelBase
 {
-    public override string Name => "HealthScene"; 
+    public override string Name => "Health"; 
+    
+    public ISeries[] Series { get; set; } = [
+        new ColumnSeries<int>(3, 4, 2),
+        new ColumnSeries<int>(4, 2, 6),
+        new ColumnSeries<double, DiamondGeometry>(4, 3, 4)
+    ];
 }
